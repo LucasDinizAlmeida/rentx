89cbc841ddfac603e3f52fd2a883c663fe206e7b
Lucas Diniz Almeida <almeidalucas820@gmail.com>
2022-10-28 09:48:54 -0300

    Creating .env file for sensitive data

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
+const WINDOWS_IP = process.env.WINDOWS_IP
 
 export const api = axios.create({
-  baseURL: 'http://10.3.152.199:3333'
+  baseURL: `http://${WINDOWS_IP}:3333`
 })
